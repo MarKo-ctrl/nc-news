85c2a227b0ac6b004a97179172959f2af73ad72b
Marios Kontis <kontism@gmail.com>
2022-06-17 11:04:35 +0100

    Add some style to Comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getComments } from '../utils/api';
+import { extractDate } from '../utils/helpers';
 
 
 export const Comments = () => {
@@ -18,22 +19,29 @@ export const Comments = () => {
 
     return (
         <>
-            <section className="accordion" id="accordionExample">
-                <div className="accordion-item">
-                    <h2 className="accordion-header" id="headingOne">
-                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            Comments ({articleComments.length})
-                        </button>
-                    </h2>
-                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                        <div className="accordion-body">
-                            {articleComments.map((comment) => {
-                                return (<>
-                                    <article>{comment.author}</article>
-                                    <article>{comment.body}</article>
-                                </>)
-                            })}
-                        </div>
+            <section className='container mt-5'>
+                <div className="accordion" id="accordionExample">
+                    <div className="row  d-flex justify-content-center">
+                        <section className="accordion-item">
+                            <h2 className="accordion-header mt-4 me-4" id="headingOne">
+                                <button className="accordion-button " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    Comments ({articleComments.length})
+                                </button>
+                            </h2>
+                            <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div className="accordion-body">
+                                    {articleComments.map((comment) => {
+                                        return (<>
+                                            <article className='d-flex card p-3 mt-3'>
+                                                <h5>{comment.author}</h5>
+                                                <p>{extractDate(comment.created_at)}</p>
+                                                <p>{comment.body}</p>
+                                            </article>
+                                        </>)
+                                    })}
+                                </div>
+                            </div>
+                        </section>
                     </div>
                 </div>
             </section>
