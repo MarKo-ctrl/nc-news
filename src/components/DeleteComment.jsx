d04328a06c1dc50a7be7f1b7d6bd0f9ec11acb93
Marios Kontis <kontism@gmail.com>
2022-08-31 10:27:54 +0100

    delete unused variable

--- src/components/DeleteComment.jsx
+++ src/components/DeleteComment.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useState } from 'react'
 import { deleteComment } from '../utils/api'
 
 export const DeleteComment = (props) => {
