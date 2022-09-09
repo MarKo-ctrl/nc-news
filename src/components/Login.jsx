4733790a0a080aed2d8ea00108521430b37bf988
Marios Kontis <kontism@gmail.com>
2022-09-09 10:40:06 +0100

    update to allow user sign in

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,62 +1,79 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 // import ReactDOM from 'react-dom'
 // import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/User';
-import { getUsers } from '../utils/api';
+import { signin } from '../utils/api';
 import { LoadingSpinner } from './LoadingSpinner';
 
 export const Login = () => {
-    const [input, setInput] = useState('')
-    const [usersList, setUsersList] = useState([])
-    const [isSubmitted, setIsSubmitted] = useState(false)
-    const [error, setError] = useState({})
-    const [isLoading, setIsLoading] = useState(false);
-    const { user, setUser } = useContext(UserContext);
+  const [input, setInput] = useState({})
+  const [isSubmitted, setIsSubmitted] = useState(false)
+  const [error, setError] = useState({})
+  const [isLoading, setIsLoading] = useState(false);
+  const { user, setUser } = useContext(UserContext);
 
-    useEffect(() => {
-        getUsers()
-            .then((users) => {
-                setUsersList(users)
-                setIsLoading(false)
-            })
-    }, [])
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        setIsSubmitted(true)
-        const validUser = usersList.find((user) => user.username === input) ? usersList.find((user) => user.username === input) : {};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsSubmitted(true);
+    setIsLoading(true)
 
-        if (Object.keys(validUser).length !== 0) {
-            setUser(validUser);
+    signin(input)
+      .then((validUser) => {
+        setIsLoading(false)
+
+        if (validUser) {
+          setUser(validUser);
         } else {
-            setError({ username: "invalid username" })
+          setError({ errMsg: 'invalid username or password' })
         }
+      })
+  }
+
+  if (isLoading) return <LoadingSpinner />;
+  return (<>
+    {!isSubmitted ? <>
+      <main>
+        <h2 className='ms-5'>User Login</h2>
+        <form id='login_form'>
+          <div className='mb-3 mx-5'>
+            <label htmlFor='username' className='form-label'>Username:</label>
+            <input
+              type='text'
+              className='form-control'
+              id='username'
+              name='username'
+              placeholder='Your Username'
+              onChange={(event) => setInput({ ...input, [event.target.name]: event.target.value })}
+              required
+            />
+          </div>
+          <div className='mb-3 mx-5'>
+            <label htmlFor='password' className='form-label'>Password:</label>
+            <input
+              type='text'
+              className='form-control'
+              id='password'
+              name='password'
+              placeholder='Your Password'
+              onChange={(event) => setInput({ ...input, [event.target.name]: event.target.value })}
+              required
+            />
+          </div>
+          <button
+            type='submit'
+            className='btn btn-primary ms-5'
+            onClick={handleSubmit}>&lt; Login &gt;
+          </button>
+        </form>
+      </main>
+    </> : isSubmitted && Object.keys(user).length !== 0 ?
+      <main>
+        <p className='success-msg'>You have successfuly logged in <strong>{user.username}</strong></p>
+      </main> :
+      <main>
+        <p className='error-msg'>{error.errMsg}</p>
+      </main>
     }
-    
-
-    if (isLoading) return <LoadingSpinner />;
-    return (<>
-        {!isSubmitted ? <>
-            <main>
-                <h2 className='ms-5'>User Login</h2>
-                <form id='login_form'>
-                    <div className="mb-3 mx-5">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Please Enter your username:</label>
-                        <input type="text" className="form-control" aria-describedby="emailHelp" onChange={(event) => setInput(event.target.value)} />
-
-                    </div>
-                    <button type="submit" className="btn btn-primary ms-5" onClick={handleSubmit}>&lt; Login &gt;</button>
-                </form>
-            </main>
-        </> : isSubmitted && Object.keys(user).length !== 0 ?
-            <main>
-                <p className='success-msg'>You have successfuly logged in <strong>{user.username}</strong></p>
-            </main> :
-            <main>
-                <p className='error-msg'>{error.username}</p>
-            </main>
-
-
-        }
-    </>)
+  </>)
 }
